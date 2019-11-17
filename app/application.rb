ea1b1356fcd2f79414c7dc06c55b1cf9af3ae76f
Allen Shin <geeuho@gmail.com>
2019-11-16 18:43:09 -0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,26 @@
 class Application
     def call(env)
-        @@items = []
         resp = Rack::Response.new
         req = Rack::Request.new(env)
 
         if req.path.match(/items/)
-            search_term = req.params["items"]
-            if @@items.include?(search_term)
-                puts @@items
-                resp.write "Item found"
+            search_word = req.path.split("/items/").last
+            puts search_word
+            item_names = @@items.map { |item| item.name}
+            puts item_names
+            if item_names.include?(search_word)
+                puts search_word
+                item = @@items.find{|item| item.name == search_word }
+                resp.write "#{item.price}"
                 resp.status = 200
             else
                 resp.write "Item not found"
                 resp.status = 400
             end
         else 
-            req.write "Route not found"
-            req.status = 404
+            resp.write "Route not found"
+            resp.status = 404
+        end
+        resp.finish
+    end
+end
